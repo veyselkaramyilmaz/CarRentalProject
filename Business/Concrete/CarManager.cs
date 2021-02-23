88855240166e84bfc9a16bffbea127fca132f36b
Veysel Karam YILMAZ <veyselkaramyilmaz@gmail.com>
2021-02-24 01:31:29 +0300

    was improved

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
@@ -47,21 +51,16 @@ namespace Business.Concrete
             return new SuccessDataResult<List<CarDetailDto>>( _carDal.GetCarDetails(filter));
         }
 
+
+        [ValidationAspect(typeof (CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.DailyPrice>0)
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.AddedCar);
-            }
-            else
-            {
-                return new ErrorResult(Messages.FailedCarAddOrUpdate);
-            }
            
-            
+           
+            _carDal.Add(car);
+            return new SuccessResult(Messages.AddedCar);
         }
-  
+             
 
         public IResult Update(Car car)
         {
